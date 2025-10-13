4a9b3bf4d53705a8d5db4214d547470db308a7f9
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-10-13 15:03:48 +0100

    finally working

--- freader/Views/MainWindow.axaml.cs
+++ freader/Views/MainWindow.axaml.cs
@@ -2,11 +2,12 @@ using Avalonia.Controls;
 
 namespace freader.Views;
 
-public partial class MainWindow : Window
+using ShadUI;
+
+public partial class MainWindow : Window  // ShadUI.Window, not Avalonia.Controls.Window  
 {
     public MainWindow()
     {
         InitializeComponent();
     }
-  
 }
