bfb2ff20a67461aeeb18b50f55fb0b34ca6c23db
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-10-13 15:05:12 +0100

    eh

---
+++ freader/AppExtensions.cs
@@ -0,0 +1,12 @@
+using freader.ViewModels;
+using freader.Views;
+
+namespace freader
+{
+    public static class AppExtensions
+    {
+        public static void RegisterTrayIconsEvents(this App app, MainWindow mainWindow, MainWindowViewModel viewModel)
+        {
+        }
+    }
+}
