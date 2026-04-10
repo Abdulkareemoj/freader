de226ddb4f764152022218f237f2df26370426d3
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2026-04-10 13:10:00 +0100

    feat: Enhance build.gradle.kts with additional repository configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,17 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    // this is necessary to avoid the plugins to be loaded multiple times
-    // in each subproject's classloader
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.composeHotReload) apply false
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.sqldelight) apply false
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+        google()
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+    }
 }
