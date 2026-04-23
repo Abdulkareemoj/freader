7312110473f53a59cf931f712a3f0faf98c081e2
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2026-04-23 15:52:59 +0100

    feat: configure coroutine delay for improved application performance

--- composeApp/src/desktopMain/kotlin/com/wiztek/freader/main.kt
+++ composeApp/src/desktopMain/kotlin/com/wiztek/freader/main.kt
@@ -5,8 +5,11 @@ import androidx.compose.ui.window.application
 import com.wiztek.freader.di.commonAppModule
 import com.wiztek.freader.di.jvmAppModule
 import org.koin.core.context.startKoin
-
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.swing.Swing
 fun main() = application {
+    System.setProperty("kotlinx.coroutines.main.delay", "true")
+
     startKoin {
         modules(commonAppModule, jvmAppModule)
     }
