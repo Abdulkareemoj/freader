1fc8560fcd5024a7ceb5a24713008c7876f08b7d
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2026-04-23 15:52:41 +0100

    feat: add LibraryImporter and BookMetadataExtractor to jvmAppModule for improved library management

--- composeApp/src/desktopMain/kotlin/com/wiztek/freader/di/JvmAppModule.kt
+++ composeApp/src/desktopMain/kotlin/com/wiztek/freader/di/JvmAppModule.kt
@@ -3,6 +3,10 @@ package com.wiztek.freader.di
 import com.wiztek.freader.database.DatabaseDriverFactory
 import com.wiztek.freader.database.createDatabase
 import com.wiztek.freader.library.repository.LibraryRepository
+import com.wiztek.freader.library.LibraryImporter
+import com.wiztek.freader.library.LibraryImporterImpl
+import okio.FileSystem
+import okio.Path.Companion.toPath
 import org.koin.dsl.module
 
 val jvmAppModule = module {
@@ -10,5 +14,18 @@ val jvmAppModule = module {
         val driverFactory = DatabaseDriverFactory()
         val database = createDatabase(driverFactory)
         LibraryRepository(database)
+    }
+
+    single<LibraryImporter> {
+        val userHome = System.getProperty("user.home")
+        val appDataDir = userHome.toPath().div(".freader")
+        LibraryImporterImpl(
+            fileSystem = FileSystem.SYSTEM,
+            appStorageDir = appDataDir
+        )
+    }
+
+    single<com.wiztek.freader.library.BookMetadataExtractor> {
+        com.wiztek.freader.library.BookMetadataExtractor()
     }
 }
