b06994d039121e8d84bf7392c9cb7440aef82e47
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2026-04-23 15:54:15 +0100

    feat: add shared dependencies for DiscoverScreenModel, StatsScreenModel, SearchScreenModel, and CollectionsViewModel

--- composeApp/src/commonMain/kotlin/com/wiztek/freader/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/com/wiztek/freader/di/AppModule.kt
@@ -9,4 +9,8 @@ interface AppModule {
 
 val commonAppModule = module {
     // Shared dependencies
+    single { com.wiztek.freader.ui.screens.discover.DiscoverScreenModel(get(), get(), get()) }
+    single { com.wiztek.freader.ui.screens.stats.StatsScreenModel(get()) }
+    single { com.wiztek.freader.ui.screens.search.SearchScreenModel(get()) }
+    single { com.wiztek.freader.ui.screens.collections.CollectionsViewModel(get()) }
 }
