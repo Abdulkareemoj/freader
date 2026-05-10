f634596a7c86e5970cf1df150872f168a314ff64
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2026-05-10 09:40:02 +0100

    feat: refactor MainActivity and main.kt to use AppCompatActivity and initialize SettingsManager

--- composeApp/src/androidMain/kotlin/com/wiztek/freader/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/wiztek/freader/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.wiztek.freader
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
+import androidx.appcompat.app.AppCompatActivity
 import com.wiztek.freader.di.androidAppModule
 import com.wiztek.freader.di.commonAppModule
+import com.wiztek.freader.settings.SettingsManager
+import com.wiztek.freader.settings.SettingsPersistence
+import org.koin.android.ext.android.get
+import org.koin.core.context.startKoin
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
@@ -18,6 +19,9 @@ class MainActivity : ComponentActivity() {
             androidContext(this@MainActivity)
             modules(commonAppModule, androidAppModule)
         }
+
+        // Initialize Settings
+        SettingsManager.init(get<SettingsPersistence>())
 
         setContent {
             App()
--- composeApp/src/desktopMain/kotlin/com/wiztek/freader/main.kt
+++ composeApp/src/desktopMain/kotlin/com/wiztek/freader/main.kt
@@ -4,15 +4,21 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.wiztek.freader.di.commonAppModule
 import com.wiztek.freader.di.jvmAppModule
+import com.wiztek.freader.settings.SettingsManager
+import com.wiztek.freader.settings.SettingsPersistence
 import org.koin.core.context.startKoin
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.swing.Swing
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.get
+
 fun main() = application {
     System.setProperty("kotlinx.coroutines.main.delay", "true")
 
-    startKoin {
+    val koinApp = startKoin {
         modules(commonAppModule, jvmAppModule)
     }
+
+    // Initialize Settings
+    SettingsManager.init(koinApp.koin.get<SettingsPersistence>())
     
     Window(
         onCloseRequest = ::exitApplication,
